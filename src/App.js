fa3a495b08a3498bd6329d33914a28efee9b3c41
axell0325 <103546414+axell0325@users.noreply.github.com>
2024-06-25 17:54:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import { Shop } from './pages/shop/shop';
 import { Cart } from './pages/cart/cart';
 import { ShopContextProvider } from './context/shop-context';
 
+import "./App.css";
+import Form from "./components/Form";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+
+
 function App() {
   const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    
     <div className="App">
       <ShopContextProvider>
         <Router>
