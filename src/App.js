081336876b9445d2a85533048873765c42a6d4e4
Staggke <staggke@gmail.com>
2024-07-09 20:09:18 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
+import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { Navbar } from './components/navbar';
 import { Shop } from './pages/shop/shop';
 import { Cart } from './pages/cart/cart';
+import { ProductPage } from './pages/productPage/productPage';
 import { ShopContextProvider } from './context/shop-context';
 
-import "./App.css";
-import Form from "./components/Form";
-import { createContext, useState } from "react";
-import ReactSwitch from "react-switch";
-
-
 function App() {
-  const [theme, setTheme] = useState("dark");
-
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
-    
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-    <div className="App" id={theme}>
-        <Form />
-        <div className="switch">
-          <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
-        </div>
-      </div>
-    </ThemeContext.Provider>
     <div className="App">
       <ShopContextProvider>
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route exact path="/" element={<Shop />} />
+            <Route exact path="/cart" element={<Cart />} />
+            <Route exact path="/:productId" element={<ProductPage />} />
           </Routes>
         </Router>
       </ShopContextProvider>
