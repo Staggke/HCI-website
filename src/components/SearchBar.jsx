89409f52d9c272c7bb4ea508d2b0ab60a35d0b69
axell0325 <103546414+axell0325@users.noreply.github.com>
2024-07-23 12:36:11 -0400

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useSate} from 'react';
 
 import {FaSearch} from "react-icons/fa"
+import "./SearchBar.css"
 
 export const SearchBar = () => {
+  const[input, setInput] = useState("e.target.value");
   return (
     <div className= "input-wrapper">
       <FaSearch id="search-icon" />
-      <input placeholder="Type to search..."/>
+      <input placeholder="Type to search..." value={input} onChange={(e) => setInput()/>
     </div>
   )
 }
