b01b6798c32734429477ccfef3b5722eb8dbcb62
axell0325 <103546414+axell0325@users.noreply.github.com>
2024-07-24 13:30:08 -0400

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,10 +5,23 @@ import "./SearchBar.css"
 
 export const SearchBar = () => {
   const[input, setInput] = useState("e.target.value");
+
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) => response.json()).then(json => {
+      const results = json.filter((user) => {
+        
+      })
+    });
+  }
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  }
   return (
     <div className= "input-wrapper">
       <FaSearch id="search-icon" />
-      <input placeholder="Type to search..." value={input} onChange={(e) => setInput()/>
+      <input placeholder="Type to search..." value={input} {onChange={(e) => handleChange(e.target.value)}/>
     </div>
   )
 }
