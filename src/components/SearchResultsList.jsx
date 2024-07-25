91c10fc75333ad50219f0d1b379ee5236aa58c65
axell0325 <103546414+axell0325@users.noreply.github.com>
2024-07-25 14:15:33 -0400

    Create SearchResultsList.jsx

---
+++ src/components/SearchResultsList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const SearchResultsList = () => {
+  return(
+    <div = className="results-list">
+      <div>A</div>
+      <div>B</div>
+      <div>C</div>
+      
+      <div/>
+    
+  );
+    
+};
